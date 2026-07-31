c3f9f28048171b2277e724adba2d74c1ee3418fb
Gled04867 <Gled04867@ya.ru>
2026-07-31 10:45:13 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Gleb on 31.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
